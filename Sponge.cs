b25223f1756dbfedec40152b36166bdca12a3c7d
Isaac Davis <msipp27@gmail.com>
2024-08-17 00:23:20 -0400

    Add files via upload

--- Sponge.cs
+++ Sponge.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.UI;
 using UnityEngine;
 
@@ -9,67 +10,98 @@ public class Sponge : MonoBehaviour
     public bool oneDir = false;
     public float size = 1f;
     public float maxsize = 10f;
+    public float growSpeed = 3f;
+
+    //is the sponge growing;
+    public bool isGrowing = false;
+
     //if the sponge only grows in one scale, does it grow up/down or left/right?
     public bool isy = true;
 
-    public float getSize(){
+    public float getSize()
+    {
         return size;
     }
 
-    public void increaseSize(float s){
+    public void increaseSize(float s)
+    {
         size += s;
     }
 
-    public void decreaseSize(float s){
+    public void decreaseSize(float s)
+    {
         size -= s;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        isGrowing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ((isGrowing == true) && (gameObject.transform.localScale.x <= 2))
+        {
+            gameObject.transform.localScale = new Vector3(transform.localScale.x + (growSpeed * Time.deltaTime), transform.localScale.y + (growSpeed * Time.deltaTime));
+            Debug.Log(gameObject.transform.localScale.magnitude);          
+        }
+        else if ((isGrowing == true) && (gameObject.transform.localScale.x == 2))
+        {
+            isGrowing = false;
+        }
     }
 
 
-    void OnCollisionEnter2D(Collision2D coll){
-        if (coll.gameObject.tag == "Player"){
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
             PlayerController p = coll.gameObject.GetComponent<PlayerController>();
 
-            if (p != null){
-                if(p.size > 1f){
+            if (p != null)
+            {
+                if (p.size > 1f && p.size - size >= size)
+                {
                     //change below later so player size is reduced to 1 or reduced by the maxsize
-                    size = p.size /2;
-                    p.decreaseSize(p.size/2);
 
-                    if(p.size < 1){
-                        Debug.Log("Reduced to smaller than size 1");
-                    }
-
-                    if(size > maxsize){
+                    if (size > maxsize)
+                    {
                         // prevents size from being lost to the ether, can be removed when inital size increase is changed.
                         p.increaseSize(size - maxsize);
                         size = maxsize;
+                        
+                    }
+                    else
+                    {
+                        size += p.size - (p.size - 1);
+                        p.size = p.size - (p.size - 1);
                     }
 
+                    if (p.size < 1)
+                    {
+                        Debug.Log("Reduced to smaller than size 1");
+                    }
 
-                    if(oneDir){
-                        if(isy){
-                        gameObject.transform.localScale = new Vector3(1, size);
-                        }else{
-                        gameObject.transform.localScale = new Vector3(size, 1);
+                    if (oneDir)
+                    {
+                        if (isy)
+                        {
+                            gameObject.transform.localScale = new Vector3(1, size);
                         }
-                    }else{
-                    gameObject.transform.localScale = new Vector3(size, size);
+                        else
+                        {
+                            gameObject.transform.localScale = new Vector3(size, 1);
+                        }
+                    }
+                    else
+                    {
+                        isGrowing = true;
                     }
 
 
-            }
+                }
 
             }
         }
